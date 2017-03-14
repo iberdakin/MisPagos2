36e35fe88a80b97bcd0bfb92f3627892c58f6efd
Gaston Avila <avila.gas@gmail.com>
2017-03-14 12:24:10 -0300

    limpieza de espaciado

--- MisPagos2/mispagos.py
+++ MisPagos2/mispagos.py
@@ -6,7 +6,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,\
 render_template, flash
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.config.from_object(__name__) #Load config from this file
 
 # Load default config and override config from an environment variable
@@ -18,37 +18,42 @@ app.config.update(dict(
 	))
 app.config.from_envvar('MISPAGOS_SETTINGS', silent=True)
 
+
 def connect_db():
 		"""Connects to the specific database"""
 		rv = sqlite3.connect(app.config['DATABASE'])
 		rv.row_factory = sqlite3.Row
 		return rv
-		
+
+
 def get_db():
 	"""Opens a new database connection if there is none yet for the current app context.
 	"""
 	if not hasattr(g,'sqlite_db'):
 		g.sqlite_db = connect_db()
 		return g.sqlite_db
-		
+
+
 @app.teardown_appcontext
 def close_db(error):
 	""" Closes the database again at the end of the request. """
 	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()				
+		g.sqlite_db.close()
 
-		
+
 @app.route('/show')
 def show_pagosmensuales():
 	db = get_db()
-	cur = db.execute('SELECT name_p, vencimiento, valor FROM pagosmensuales\
-					  INNER JOIN mispagos ON id_pago=id_p')
+	cur = db.execute('SELECT name_p, vencimiento, valor FROM pagosmensuales'
+					 'INNER JOIN mispagos ON id_pago=id_p')
 	pagos = cur.fetchall()
 	return render_template('show_pagos.html',pagos=pagos)
-	
+
+
 @app.route('/new')
 def new_pago():
 	return render_template('new_pago.html')	
+
 
 @app.route('/add', methods=['POST'])
 def add_pago():
@@ -64,7 +69,8 @@ def add_pago():
 	db.commit()
 	flash('New entry was succesfully posted')
 	return redirect(url_for('show_pagosmensuales'))
-	
+
+
 @app.route('/login', methods=['GET','POST'])
 def login():
 	error = None
@@ -79,8 +85,10 @@ def login():
 			return redirect(url_for('show_pagosmensuales'))
 	return render_template('login.html', error=error)
 
+
 @app.route('/logout')
 def logout():
 	session.pop('logged_in',None)
 	flash('You were logged out')
-	return redirect(url_for('login'))		
+	return redirect(url_for('login'))
+
