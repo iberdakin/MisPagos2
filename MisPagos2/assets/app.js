0aec242e5590698797f29354854fcddc31e88f9c
Ivan Berdakin <iberdakin@gmail.com>
2017-04-09 18:51:24 -0300

    Enrutamiento solucionado. Se agrega formulario para agregar nuevos pagos

--- MisPagos2/assets/app.js
+++ MisPagos2/assets/app.js
@@ -1,8 +1,64 @@
 var angular = require('angular');
+var ngRute = require('angular-route');
 
 var app = angular.module('app', [
-    require('angular-route'),
-    require('angular-ui-bootstrap'),
+    'ngRoute'
 ]);
 
-require('./components/pagos-list/pagos-list')(app);
+angular.module('app').config(['$locationProvider','$routeProvider',
+    function config($locationProvider,$routeProvider){
+            $locationProvider.hashPrefix('!');
+            $routeProvider.
+               when('/pagosList', {
+                template: require('./components/pagos-list/pagos-list.html')//,
+//                controller: 'pagosCtrl'
+            }).when('/pagosAdd',{
+                template: require('./components/pagos-add/pagos-add.html')//,
+//                controller: 'pagosCtrl'                
+            }).otherwise('pagosList');
+  }]
+);
+
+app.controller('pagosCtrl',['$scope',
+    function($scope){
+        $scope.pagos = [
+        {
+            name: 'EPEC',
+            value: 147,
+            month: 'enero'
+        },{
+            name: 'EPEC',
+            value: 148,
+            month: 'febrero'
+        },{
+            name: 'ECOGAS',
+            value: 247,
+            month: 'enero'
+        },{
+            name: 'ECOGAS',
+            value: 248,
+            month: 'febrero'
+        }
+        ];               
+        console.log($scope.pagos)
+        
+        $scope.newpago = {
+            name: '',
+            value: 0,
+            month: ''
+        };
+        
+        $scope.update = function(newpago){
+            console.log(newpago);
+            nuevo = {
+            name: newpago.name,
+            value: newpago.value,
+            month: newpago.month
+        }; 
+            $scope.pagos.push(nuevo);
+            
+        }
+        console.log($scope.pagos)
+        
+    }                              
+]);
